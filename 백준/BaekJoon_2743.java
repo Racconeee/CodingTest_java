fc59b79f811a48d72dfd9ab19a014ff80c4eda5f
Racconeee <raccon6614@gmail.com>
2023-06-22 23:21:03 +0900

    0622

--- 백준/BaekJoon_2743.java
+++ 백준/BaekJoon_2743.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 2743 {
+public class BaekJoon_2743 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
