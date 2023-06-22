851317e709cfe85a343877bcc651efb868e1d66c
Racconeee <raccon6614@gmail.com>
2023-06-22 23:20:32 +0900

    0622

--- 백준/BaekJoon_27866.java
+++ 백준/BaekJoon_27866.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class 27866 {
+public class BaekJoon_27866 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
