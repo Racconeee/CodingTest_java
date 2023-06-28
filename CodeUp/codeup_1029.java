eac46e768444250c3330e6ee7e12caf83ab128ba
Racconeee <raccon6614@gmail.com>
2023-06-28 23:52:12 +0900

    실수 1개 입력받아 그대로 출력하기2

---
+++ CodeUp/codeup_1029.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class codeup_1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double n = sc.nextDouble();
+
+        System.out.printf("%.11f",n);
+
+    }
+}
