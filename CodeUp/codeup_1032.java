a5cf84bb9a9909de6a1951baf566a2a62c045ffe
Racconeee <raccon6614@gmail.com>
2023-06-28 23:57:04 +0900

    10진 정수 입력받아 16진수로 출력하기1

---
+++ CodeUp/codeup_1032.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class codeup_1032 {
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(Integer.toString(n,16));
+    }
+}
