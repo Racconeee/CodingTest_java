fbebccb5be7c40d979c53b935bea66a90918b1e2
Racconeee <raccon6614@gmail.com>
2023-06-30 14:13:53 +0900

    8진 정수 1개 입력받아 10진수로 출력하기

---
+++ CodeUp/codeup_1034.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class codeup_1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(Integer.parseInt(str,8));
+    }
+}
