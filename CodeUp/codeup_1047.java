d08d35ecb5cefada2e3d36b149cfda311d60e82b
Racconeee <raccon6614@gmail.com>
2023-07-01 13:43:31 +0900

    정수 1개 입력받아 2배 곱해 출력하기

---
+++ CodeUp/codeup_1047.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class codeup_1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n<<1);
+    }
+}
