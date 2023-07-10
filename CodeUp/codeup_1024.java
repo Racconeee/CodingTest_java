7790da2b132192e17b99bf3ea43e03d32bd0ef31
Racconeee <raccon6614@gmail.com>
2023-07-10 22:27:40 +0900

    Raccon

--- CodeUp/codeup_1024.java
+++ CodeUp/codeup_1024.java
@@ -5,12 +5,10 @@ import java.io.InputStreamReader;
 public class codeup_1024 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
         String str = br.readLine();
 
         for (int i = 0; i < str.length(); i++) {
-            System.out.println("'" + str.charAt(i) + "'");
+            System.out.println("'" + str.charAt(i)  + "'");
         }
         
 
