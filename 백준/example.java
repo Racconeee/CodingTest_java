6f8051aa3f968fe3ba7ca52a9d8fbb0ce5906a58
Racconeee <raccon6614@gmail.com>
2024-07-01 23:45:07 +0900

    240701

--- 백준/example.java
+++ 백준/example.java
@@ -1,24 +1,29 @@
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class example {
     public static void main(String[] args){
-            int[] prices = {1,2,3,2,3};
-            int[] answer = new int[prices.length];
-            Stack<Integer[]> stack = new Stack<>();
-            
-            for(int i = 0; i < prices.length; i++){
-                answer[i] = answer.length - 1 - i; // 최대기간으로 세팅
-                Integer[] arr = {i, prices[i]}; // 인덱스, 가격
-                
-                while(!stack.empty() && stack.peek()[1] > prices[i]){ // 가격이 떨어진 경우
-                    Integer[] price = stack.pop();
-                    answer[price[0]] = i - price[0];
-                }
-                System.out.println("Stack: " + Arrays.deepToString(stack.toArray()));
-                stack.push(arr);
+        
+
+        int[] arrays = {1,1,3,3,0,1,1};
+
+
+        Queue<Integer> queue = new LinkedList<>();
+
+
+        queue.add(arrays[0]);
+        for (int i = 1; i < arrays.length; i++) {
+            if (arrays[i-1] != arrays[i]) {
+                queue.add(arrays[i]);
             }
-            
-            System.out.println(Arrays.toString(answer));
-}
+            System.out.println(queue.peek());
+        }
+        
+        int[] answer = new int[queue.size()];
+        int index = 0;
+        while (queue.size() >= 1) {
+            answer[index++] = queue.poll();
+        }
+    }
 }
---
+++ 프로그래머스/code.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class code{
+    
+
+    public static void main(String[] args) {
+    
+
+        int n = 118372;
+        
+        String str = String.valueOf(n);
+        String tmp = "";
+        int l
+        for (int i = 0; i < args.length; i++) {
+            l = str.charAt(i)
+            for (int j = 0; j < args.length; j++) {
+                if()
+            }
+        }
+    }
+
+
+}
